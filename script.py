39b122bd1f36260154ca0b6f4c2c9992e2f4a121
Sid Kanderi <126908927+skanderi7@users.noreply.github.com>
2024-03-18 15:10:28 -0400

    Update script.py

--- script.py
+++ script.py
@@ -26,17 +26,10 @@ def scrape_data_point():
 
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
-        # target_element = soup.find("a", class_="frontpage-link")
-        most_read_item_div = soup.find("div", class_="col-sm-5 most-read-item")  # Locate the div
-        if most_read_item_div:
-            # Search for the target element within the div
-            target_element = most_read_item_div.find("a", class_="frontpage-link")
-            data_point = "" if target_element is None else target_element.text
-            loguru.logger.info(f"Data point: {data_point}")
-            return data_point
-        # data_point = "" if target_element is None else target_element.text
-        # loguru.logger.info(f"Data point: {data_point}")
-        # return data_point
+        target_element = soup.find("a", class_="frontpage-link")
+        data_point = "" if target_element is None else target_element.text
+        loguru.logger.info(f"Data point: {data_point}")
+        return data_point
 
 
 if __name__ == "__main__":
