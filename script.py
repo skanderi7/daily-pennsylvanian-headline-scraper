cf3a7264289954c4e55f70a42f7db6f373b18d35
Sid Kanderi <126908927+skanderi7@users.noreply.github.com>
2024-03-18 16:05:57 -0400

    Update script.py

--- script.py
+++ script.py
@@ -20,13 +20,13 @@ def scrape_data_point():
     Returns:
         str: The headline text if found, otherwise an empty string.
     """
-    req = requests.get("https://www.thedp.com")
+    req = requests.get("https://www.thedp.com/sections/podcasts")
     loguru.logger.info(f"Request URL: {req.url}")
     loguru.logger.info(f"Request status code: {req.status_code}")
 
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
-        target_element = soup.find("a", class_="frontpage-link")
+        target_element = soup.find("h3", class_="standard-link")
         data_point = "" if target_element is None else target_element.text
         loguru.logger.info(f"Data point: {data_point}")
         return data_point
